d0721f5c4e8791cae570601d8441502f8d6241b4
ErinyAdel <renyadel7@gmail.com>
2021-12-18 18:50:30 +0200

    Finished

--- LDA -Topic Modeling (EN)/LDA.py
+++ LDA -Topic Modeling (EN)/LDA.py
@@ -12,32 +12,31 @@ from sklearn.decomposition import LatentDirichletAllocation
 npr = pd.read_csv('npr.csv')
 print(npr.head())
 
-cv = CountVectorizer(max_df=0.95, min_df=2, stop_words='english')
+cv = CountVectorizer(max_df=0.95, min_df=2, stop_words='english')  ## max_df: Maximum Document Frequency
 dtm = cv.fit_transform(npr['Article'])
 
+LDA = LatentDirichletAllocation(n_components=7,random_state=42)
+LDA.fit(dtm)
+
+## Number Of (Unique) Words In All Articles.
 print(len(cv.get_feature_names()))
 
-
+print("-- Random 10 Words --")
 for i in range(10):
     random_word_id = random.randint(0,54776)
     print(cv.get_feature_names()[random_word_id])
     
 
-LDA = LatentDirichletAllocation(n_components=7,random_state=42)
-LDA.fit(dtm)
-
-print(len(LDA.components_))
-print(LDA.components_)
-print(len(LDA.components_) , len(LDA.components_[0]))
+print(len(LDA.components_), '\n')    ## No. of Components
+print(LDA.components_, '\n')         ## The Components
+print(len(LDA.components_), '\n')    ## Len of First Component
+print(len(LDA.components_[0]), '\n') ## First Component 
 
 
 single_topic = LDA.components_[0]
 single_topic.argsort()
 
 single_topic.argsort()[-10:]
-
-for index in single_topic.argsort()[-10:]:
-    print(cv.get_feature_names()[index])
     
 for index,topic in enumerate(LDA.components_):
     print(f'THE TOP 15 WORDS FOR TOPIC #{index}')
@@ -46,7 +45,6 @@ for index,topic in enumerate(LDA.components_):
 topic_results = LDA.transform(dtm)
 topic_results.shape
 topic_results[0].round(2)
-topic_results.argmax(axis=1)
 
 npr['Topic'] = topic_results.argmax(axis=1)
-npr.head(10)
+print(npr.head(10))
